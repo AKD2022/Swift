e6693ad01c669b1b381ffcd67adbf716969224cf
AKD <akdusa20@gmail.com>
2023-06-09 20:53:06 -0400

    Added Files

---
+++ Scrumdinger/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by AK D on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
